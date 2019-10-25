d8018fe148889cd21ebd44bb24ee63371029bb4f
BarisJKC <baris.gulseren@gmail.com>
2019-10-25 10:05:10 +0300

    add cors lib

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 // const config = require("config");
 const mongodbURL = require("./config/mongodb");
+const cors = require("cors");
 
 const moviesRoutes = require("./routes/movies");
 const actorsRoutes = require("./routes/actors");
@@ -17,7 +18,7 @@ mongoose
     .catch(err => console.log("Error",err.message));
 
 app.use(express.json()); //Accept json format for request body
-
+app.use(cors()); // allow cross origin request for ajax calls, cors içindeki parantezlere belirli adresleri listelerek giriş izni verir. netlify adresi gib. spacex gibi herkese açık verenler buraya liste koymuyor
 app.use("/movies",moviesRoutes);
 app.use("/actors",actorsRoutes);
 
